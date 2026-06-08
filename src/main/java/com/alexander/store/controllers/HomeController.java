eba93b7312626e3d3f4bbe1f0f09d96cdc11b3fd
Isaac Alexander <Isaacalexander81@gmail.com>
2026-06-08 02:00:17 +0100

    building an api

--- src/main/java/com/alexander/store/controllers/HomeController.java
+++ src/main/java/com/alexander/store/controllers/HomeController.java
@@ -1,17 +1,16 @@
 package com.alexander.store.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
-    }
+    public String index(Model model) {
 
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "index.html";
+        model.addAttribute("name", "Alex");
+
+        return "index";
     }
 }
