15abcebe0497c52e960a1c2145fa015c7e8c06d7
Isaac Alexander <Isaacalexander81@gmail.com>
2026-06-09 02:01:31 +0100

    setting HTTP status and using DTO

--- src/main/java/com/alexander/store/controllers/UserController.java
+++ src/main/java/com/alexander/store/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.alexander.store.controllers;
 
+import com.alexander.store.dtos.UserDto;
 import com.alexander.store.entities.User;
 import com.alexander.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +15,26 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    @GetMapping
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+           return ResponseEntity.notFound().build();
+        }
+
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
